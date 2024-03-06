8b07c9ebe0d01eba6c973671154d097d513c319f
devbyul <devbyul@gmail.com>
2024-03-06 13:19:57 +0900

    :sparkles:feat: Service & Custom Error Builder 적용

--- app/src/domain/controller/world.controller.ts
+++ app/src/domain/controller/world.controller.ts
@@ -1,19 +1,25 @@
+import { StatusCodes } from "http-status-codes";
+import ErrorBuilder from "../../core/config/error/error";
 import { IController } from "../../core/interface/IExpress";
+import WorldService from "../service/world.service";
 import BaseController from "./base.controller";
 
 export default class WorldController extends BaseController {
   static helloWorld: IController = (req, res) => {
-    BaseController.json(res, 200, "Hello World");
+    const result = WorldService.getHelloWorld();
+
+    BaseController.json(res, 200, result);
   };
 
   static errorWorld: IController = (req, res) => {
     try {
-      throw new Error("Error World");
+      throw new ErrorBuilder()
+        .setCode("ErrorWorld")
+        .setStatus(StatusCodes.BAD_REQUEST)
+        .setMessage("Error World!")
+        .build();
     } catch (error: any) {
-      BaseController.error(res, 400, {
-        code: "Error World",
-        message: "Error World Msg",
-      });
+      BaseController.error(res, error?.status, error);
     }
   };
 }

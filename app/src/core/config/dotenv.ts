60d9d1eb0aa2083a5f9e05b0bae82cec0d1f817c
devbyul <devbyul@gmail.com>
2024-03-06 09:20:34 +0900

    :book:config: dotenv 모듈화

---
+++ app/src/core/config/dotenv.ts
@@ -0,0 +1,27 @@
+import { config } from "dotenv";
+
+let path;
+switch (process.env.NODE_MODE) {
+  case "local":
+    path = `${process.cwd()}/app/env/.env.local`;
+    break;
+  case "dev":
+    path = `${process.cwd()}/app/env/.env.dev`;
+    break;
+  case "prod":
+    path = `${process.cwd()}/app/env/.env.prod`;
+    break;
+}
+
+config({ path });
+
+const dotenv = {
+  node: {
+    mode: process.env.NODE_MODE,
+  },
+  server: {
+    port: process.env.PORT,
+  },
+};
+
+export default dotenv;

d8f39f88f986fb497f56762b7387683a79af0a75
devbyul <devbyul@gmail.com>
2024-03-07 14:34:27 +0900

    :book:config: 404 NotFound & json, urlencoded 설정

--- app/src/core/config/express.ts
+++ app/src/core/config/express.ts
@@ -8,8 +8,15 @@ import WorldRouter from "../../domain/route/world/index";
 
 const app: Express = express();
 
-app.use(morgan("combined", { stream }));
+/**
+ * JSON & URLencoded
+ */
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
+/**
+ * CORS
+ */
 const originList = ["http://127.0.0.1:3001", "http://localhost:3001"];
 const corsOption: CorsOptions = {
   origin: originList,
@@ -20,6 +27,21 @@ const corsOption: CorsOptions = {
 };
 app.use(cors(corsOption));
 
+/**
+ * Logger [ morgan ]
+ */
+app.use(morgan("combined", { stream }));
+
+/**
+ * Route
+ */
 app.use("/world", WorldRouter);
+
+/**
+ * Route Error NotFound
+ */
+app.use((req, res) => {
+  res.status(404).send("Not Found");
+});
 
 export default app;
